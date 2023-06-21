b1228f3f1508c496dbb24564837919577b1daacf
何易萍 <2945515376@qq.com>
2023-06-21 22:13:32 +0800

    fix:图片懒加载的实现

--- src/main.js
+++ src/main.js
@@ -2,17 +2,35 @@
 import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
-// 测试接口函数
-// import { getCategory} from '@/apis/testAPI'
-// getCategory().then(res=>{
-//     console.log(res);
-// })
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+// 定义全局指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el:：指定绑定的那个元素 img
+        // binding : bingding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding.value);
+        const { stop } = useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+              console.log(isIntersecting)
+              if (isIntersecting) {
+                // 进入视口区域
+                el.src = binding.value
+                stop()
+              }
+            },
+          )
+    }
+})
+// 
