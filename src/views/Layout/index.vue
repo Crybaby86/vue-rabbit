a69e96169c2db1b940cb31f14c0f84931f244614
何易萍 <2945515376@qq.com>
2023-06-24 15:01:14 +0800

    fix:一级导航的路由配置

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,19 +1,21 @@
 <script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
 //触发获取导航列表的action
-import { useCategoryStore } from '@/stores/category';
-import { onMounted } from 'vue';
+import { useCategoryStore } from "@/stores/category";
+import { onMounted } from "vue";
 
-const categoryStore = useCategoryStore()
+const categoryStore = useCategoryStore();
 onMounted(() => {
   // 方法调用
-  categoryStore.getCategary()
-})
+  categoryStore.getCategary();
+});
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
