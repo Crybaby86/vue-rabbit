aedf9465d94a67ca54378271c05219782e06edeb
何易萍 <2945515376@qq.com>
2023-09-04 15:39:59 +0800

    feat:tab列表数据切换

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -25,6 +25,12 @@ const getGoodList = async () => {
   goodList.value = res.result.items;
 };
 onMounted(() => getGoodList());
+
+// tab 切换
+const tabChange = () => {
+  reqData.value.page = 1;
+  getGoodList();
+};
 </script>
 
 <template>
@@ -40,7 +46,7 @@ onMounted(() => getGoodList());
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
