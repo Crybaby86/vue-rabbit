e70b7ad18d722cc1304170af736264f276d3fcce
何易萍 <2945515376@qq.com>
2023-09-04 09:37:35 +0800

    home的轮播图实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,10 +1,13 @@
 import httpInstance from "@/utils/http";
 
 // 获取banner
-export function getBannerAPI() {
-    return httpInstance({
-        url: '/home/banner'
-    })
+export function getBannerAPI(parmas = {}) {
+  const { distributionSite = "1" } = parmas;
+  // 默认为1 商品为2
+  return httpInstance({
+    url: "/home/banner",
+    distributionSite,
+  });
 }
 
 /**
@@ -13,30 +16,28 @@ export function getBannerAPI() {
  * @return {*}
  */
 export const findNewAPI = () => {
-    return httpInstance({
-        url: '/home/new'
-    })
-}
+  return httpInstance({
+    url: "/home/new",
+  });
+};
 
 /**
-* @description: 获取人气推荐
-* @param {*}
-* @return {*}
-*/
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
 export const findHotAPI = () => {
-    return httpInstance({
-        url: '/home/hot'
-    })
-}
+  return httpInstance({
+    url: "/home/hot",
+  });
+};
 /**
-* @description: 获取商品列表
-* @param {*}
-* @return {*}
-*/
+ * @description: 获取商品列表
+ * @param {*}
+ * @return {*}
+ */
 export const getGoodsAPI = () => {
-    return httpInstance({
-        url: '/home/goods'
-    })
-}
-
-
+  return httpInstance({
+    url: "/home/goods",
+  });
+};
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { getCategoryAPI } from "@/apis/category";
+import { getBannerAPI } from "@/apis/home";
 import { onMounted, onUpdated, ref } from "vue";
 import { useRoute } from "vue-router";
 const categoryData = ref({});
@@ -9,16 +10,53 @@ const getCategory = async (id) => {
   const res = await getCategoryAPI(id);
   categoryData.value = res.result;
 };
+
+// 轮播图
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI({ distributionSite: "2" });
+  bannerList.value = res.result;
+};
 onUpdated(() => {
   getCategory(route.params.id);
+  getBanner();
 });
 </script>
 
 <template>
-  <div class="bread-container">
-    <el-breadcrumb separator=">">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
-    </el-breadcrumb>
+  <div class="top-category">
+    <div class="container m-top-20">
+      <!-- 面包屑 -->
+      <div class="bread-container">
+        <el-breadcrumb separator=">">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  /* position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98; */
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
